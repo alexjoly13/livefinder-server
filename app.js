a42a1d2d79c6efe2154e9a8178d63c25383668e7
Jules Faivre <faivre.jules@gmail.com>
2019-03-06 18:26:02 +0100

    REACT FILES ADDED

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const passport = require("passport");
 require("./config/passport-setup.js");
 
 mongoose
-  .connect("mongodb://localhost/project3-server", { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then(x => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
---
+++ public/precache-manifest.da90f0dff516fad04b095c143f29f5a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1649c67460e0858fc6e8",
+    "url": "/static/js/main.1649c674.chunk.js"
+  },
+  {
+    "revision": "3d4bab861de7b9ecd4a4",
+    "url": "/static/js/2.3d4bab86.chunk.js"
+  },
+  {
+    "revision": "1649c67460e0858fc6e8",
+    "url": "/static/css/main.48217822.chunk.css"
+  },
+  {
+    "revision": "ddb2b7b9ec746cfe6dc4be6554cf4e6b",
+    "url": "/index.html"
+  }
+];
